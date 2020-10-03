d3f225857eb3390029cc5dbada4745bfe26d439f
Max Jenke <milchratchet@googlemail.com>
2020-10-03 09:34:23 +0200

    reworked and updated introduction page

--- SubBox/wwwroot/js/tutorial.js
+++ SubBox/wwwroot/js/tutorial.js
@@ -1,4 +1,8 @@
 var app = new Vue({
+    data: {
+        slide: 0,
+        lastSlideChange: null,
+    },
     methods: {
         async leave() {
             await fetch("api/values/firstdone", { method: "POST" });
@@ -13,5 +17,28 @@ var app = new Vue({
         page.addEventListener("contextmenu", function (event) {
             event.preventDefault();
         }, false);
+
+        this.lastSlideChange = Date.now();
+
+        window.addEventListener('wheel', function(event) {
+            var newTime = Date.now();
+
+            if (newTime - app.lastSlideChange > 300) {
+                if (event.deltaY > 0) {
+                    app.slide++;
+                } else {
+                    app.slide--;
+                }
+
+                if (app.slide < 0) {
+                    app.slide = 0;
+                } else if (app.slide > 5) {
+                    app.slide = 5;
+                }
+
+                app.lastSlideChange = newTime;
+            }
+
+        });
     }
 });
